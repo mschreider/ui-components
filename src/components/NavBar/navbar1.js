266e39b0af137076b131f56d8ea688c1cf8c74aa
Matthew Schreider <matt.schreider@sotertechnologies.com>
2023-02-28 10:52:11 -0500

    first puch

---
+++ src/components/NavBar/navbar1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../resources/less/NavBar/navbar1.less'
+
+function navbar1({children, ...props}) {
+    return (
+        <div className='container'>
+            
+        </div>
+    )
+}
+
+export default navbar1;
